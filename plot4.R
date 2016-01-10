ca5e4be30fe85d09abec9f285dc30c69e18816ff
Guo Fong, Luo <foreversponz@gmail.com>
2016-01-11 01:18:29 +0800

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,21 @@
+##Please put the file "household_power_consumption.txt" in the Working directory.
+p1full <- read.table("household_power_consumption.txt",stringsAsFactors=FALSE,
+                     header=TRUE, sep = ";", dec = ".")
+p1sub <- p1full[p1full$Date %in% c("1/2/2007","2/2/2007") ,]
+datetime <- strptime(paste(p1sub$Date, p1sub$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+gap <- as.numeric(p1sub$Global_active_power)
+grp <- as.numeric(p1sub$Global_reactive_power)
+voltage <- as.numeric(p1sub$Voltage)
+s1 <- as.numeric(p1sub$Sub_metering_1)
+s2 <- as.numeric(p1sub$Sub_metering_2)
+s3 <- as.numeric(p1sub$Sub_metering_3)
+png("plot4.png", width=480, height=480)
+par(mfrow = c(2, 2)) 
+plot(datetime, gap, type="l", xlab="", ylab="Global Active Power", cex=0.2)
+plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
+plot(datetime, s1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, s2, type="l", col="red")
+lines(datetime, s3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+plot(datetime, grp, type="l", xlab="datetime", ylab="Global_reactive_power")
+dev.off()
